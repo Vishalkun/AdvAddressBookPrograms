b028666d90b50808586c17e54c0899d9c29304e4
Vishal github <Vishal.kunchal@gmail.com>
2023-09-29 21:07:11 +0530

    [vishal kunchal]: Creating the database

---
+++ AddressBookOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvAddressBookSystem
+{
+    internal class AddressBookOperation
+    {
+        public static void createdatabase()
+        {
+            SqlConnection con = new SqlConnection("data source=(localdb)\\MSSQLLocalDB;initial catalog=master;integrated security=true");
+            string query = "create database Customer";
+            SqlCommand cmd = new SqlCommand(query, con);
+            //open connection
+            con.Open();
+            //Execut query
+            cmd.ExecuteNonQuery();
+            //Message
+            Console.WriteLine("Database created successfully");
+            Console.WriteLine("-----------------------------");
+            //Close connection
+            con.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvAddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
